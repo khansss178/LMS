bb4b3299b8977e797f69fb8725191d64ccd8355f
sherwani0790 <sherwani0790@gmail.com>
2024-03-20 07:47:29 -0700

    LoginView Set

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -7,6 +7,8 @@ import "./Login.css";
 import { resetChangeStatus, loginUser } from "../../redux/auth_slice/login_user_slice";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from 'react-toastify';
+import { InputText } from 'primereact/inputtext';
+import { Password } from "primereact/password";
 
 const Login = () => {
     const dispatch = useDispatch();
@@ -57,46 +59,56 @@ const Login = () => {
         return isFormFieldValid(name) && <small className="p-error">{formik.errors[name]}</small>;
     };
     return (
-        <div className="login_body">
-            <div align="center" style={{ marginTop: "4%", marginBottom: "1%" }}>
-                <img src="" alt="" width={"50%"} />
-            </div>
-            <div className="container" id="container">
-                <div className="form-container sign-in-container">
-                    <form className="login_form" onSubmit={formik.handleSubmit}>
-                        <div className="p-mb-4">
-                            <h1 className="login_h1">Login</h1>
-                        </div>
-                        <div className="p-mt-4">
-                            <div className="user_Email-Name">
-                                <label><b>Username</b></label>
-                                <input id='username' name='username' value={formik.values.username} onChange={formik.handleChange} autoFocus className="login_input" type="text" placeholder="User Name" />
-                                {getFormErrorMessage('username')}
-                            </div>
-                            <div className="user_Email-Name">
-                                <label><b>Password</b></label>
-                                <input className="login_input input_pass"
-                                    value={formik.values.password} onChange={formik.handleChange}
-                                    name="password" id="password"
-                                    type="password"
-                                    placeholder="Enter Password Here!"
-                                />
-                                {getFormErrorMessage("password")}
-                            </div>
-                            <div className="p-mt-2">
-                                <Button className="login_button" label="Login" loading={loading} type="submit" />
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <div className="overlay-container">
-                    <div className="overlay">
-                        <div className="overlay-panel overlay-right">
-                            <h1 className="login_h1">Welcome!</h1>
-                            <p className="login_p">Please login to access TEEK Portal</p>
+        <div className="background_image container-fluid">
+            <div className="grid pr-5">
+                <div className="lg:col-8 md:col-6"></div>
+                <div className="lg:col-4 md:col-6 col-12">
+                    <div className="container" style={{marginTop:"20%"}}>
+                        <p className="sign_in_text">Sign In</p>
+                        <p className="sign_text">Enter your Email & Password to Login</p>
+
+                        <div className="pt-3">
+                            <form className="login_form" onSubmit={formik.handleSubmit}>
+                                <div className="p-mb-4">
+                                    <h1 className="login_h1">Login</h1>
+                                </div>
+                                <div className="p-mt-4">
+                                    <div className="user_Email-Name  mb-3">
+
+                                        <div>
+                                            <label><b>Username</b></label>
+                                        </div>
+
+                                        <div>
+                                            <InputText id='username' name='username' value={formik.values.username} onChange={formik.handleChange} autoFocus className="login_input" type="text" placeholder="User Name" />
+                                            {getFormErrorMessage('username')}
+                                        </div>
+                                    </div>
+                                    <div className="user_Email-Name mb-3">
+                                        <div>
+                                            <label><b>Password</b></label>
+                                        </div>
+                                        <div>
+
+                                            <Password  className="login_input input_pass"
+                                                value={formik.values.password} onChange={formik.handleChange}
+                                                name="password" id="password"
+                                                type="password"
+                                                placeholder="Enter Password Here!"
+                                                toggleMask
+                                            />
+                                            {getFormErrorMessage("password")}
+                                        </div>
+                                    </div>
+                                    <div className="flex flex-row justify-content-center mt-5 mt-2">
+                                        <Button className="btn sign_in_btn" label="Login" loading={loading} type="submit" />
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
+
             </div>
 
         </div>
