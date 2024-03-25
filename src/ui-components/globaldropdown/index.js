2944a635031533d781c29a536f06a92d9a758605
Sherwani0790 <145917397+Sherwani0790@users.noreply.github.com>
2024-03-25 23:37:42 +0500

    Update index.js

--- src/ui-components/globaldropdown/index.js
+++ src/ui-components/globaldropdown/index.js
@@ -4,48 +4,22 @@ import "./globaldropdown.scss";
 
 import { Dropdown } from "primereact/dropdown";
 const GlobalDropdown = (props) => {
-  const {
-    value,
-    options,
-    onChange,
-    optionLabel,
-    placeholder,
-    name,
-    id,
-    editable = false,
-    label,
-    isRequired,
-    isDependent,
-    disabled
-  } = props;
-  return (
-    <>
-      {label && (
-        <div className="d-flex">
-          <label>
-            <b>{label}</b>
-            {(isRequired || isDependent) && (
-              <span className={`${isDependent ? "clr_green" : "clr_red"}  `}>
-                *
-              </span>
+    const { value, options, onChange, optionLabel, placeholder, name, id, editable = false, label, isRequired, isDependent, disabled, ...remainingProps } = props;
+
+    console.log({ value, optionLabel });
+    return (
+        <>
+            {label && (
+                <div className="d-flex">
+                    <label>
+                        <b>{label}</b>
+                        {(isRequired || isDependent) && <span className={`${isDependent ? "clr_green" : "clr_red"}  `}>*</span>}
+                    </label>
+                </div>
             )}
-          </label>
-        </div>
-      )}
-      <Dropdown
-        value={value}
-        options={options}
-        onChange={onChange}
-        optionLabel={optionLabel}
-        placeholder={placeholder}
-        className={"filterbar_dropdown"}
-        name={name}
-        id={id}
-        editable={editable}
-        disabled={disabled}
-      />
-    </>
-  );
+            <Dropdown value={value} options={options} onChange={onChange} optionLabel={optionLabel} placeholder={placeholder} className={"filterbar_dropdown"} name={name} id={id} editable={editable} disabled={disabled} {...remainingProps} />
+        </>
+    );
 };
 
 export default GlobalDropdown;
